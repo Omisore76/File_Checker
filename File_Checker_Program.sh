518d19d075b538ac2b02544533947cd98f9c7053
Dotun <hodote76@gmail.com>
2022-07-31 20:38:20 +0100

    File checker script

---
+++ File_Checker_Program.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Welcome to my file checker program
+
+echo "Welcome to my file checker script!"
+
+read -p "Enter source folder path: " source_folder_path
+read -p "Enter destination folder path: " destination_folder_path
+
+for file in $(find $source_folder_path -printf "%P\n"); do
+        if [ -a $destination_folder_path/$file ]; then
+                if [ $source_folder_path/$file -nt $destination_folder_path/$file ]; then
+                        echo "There is a new file"
+                        cp -r $source_folder_path/$file $destination_folder_path
+
+                else
+                echo "The file already exist, skipping this file"
+                fi
+
+        else
+                echo "copying your files to the destination folder"
+                cp -r $source_folder_path/$file $destination_folder_path
+        fi
+done
+
+
+tar czf destination_folder.tar $destination_folder_path
+
+gzip destination_folder.tar
+
+aws s3 cp destination_folder.tar.gz s3://dailybackupbucket8976/
